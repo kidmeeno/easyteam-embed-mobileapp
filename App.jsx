2d42f0d6985a957d8e2330c907c81f6e0ff21b4f
kidmeeno <arthurbosah@yahoo.com>
2024-10-21 07:18:33 +0100

    Add navigator to the app entry with the context provider for auth and few state management

--- App.jsx
+++ App.jsx
@@ -1,12 +1,52 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+// import Clock from "@/Screens/ClockScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import "react-native-url-polyfill/auto";
+import ShiftFormScreen from "./Screens/ShiftFormScreen";
+import ClockScreen from "./Screens/ClockScreen";
+import EmployeesAdminScreen from "./Screens/EmployeesAdminScreen";
+import TimesheetScreen from "./Screens/TimesheetEmployeeScreen";
+import TimesheetAdminScreen from "./Screens/TimesheetAdminScreen";
+import SettingsScreen from "./Screens/SettingsScreen";
+import LoginScreen from "./Screens/LoginScreen";
+import SignUpScreen from "./Screens/SignUpScreen";
+import { AppStateProvider } from "./context/AppStateContext";
 
-const App = () => {
-    return (
-        <View>
-           <Text>App.jsx</Text> 
-        </View>
-    );
-};
+const Stack = createNativeStackNavigator();
 
-export default App;
+export default function App() {
+  return (
+    <AppStateProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="LoginScreen"
+            component={LoginScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="SignUpScreen"
+            component={SignUpScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="ClockScreen"
+            component={ClockScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="EmployeesAdminScreen"
+            component={EmployeesAdminScreen}
+          />
+          <Stack.Screen name="TimesheetScreen" component={TimesheetScreen} />
+          <Stack.Screen
+            name="TimesheetAdminScreen"
+            component={TimesheetAdminScreen}
+          />
+          <Stack.Screen name="ShiftFormScreen" component={ShiftFormScreen} />
+          <Stack.Screen name="SettingsScreen" component={SettingsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </AppStateProvider>
+  );
+}
