27c21184f34ff1b8697dae47093c15bfd294d1bc
kidmeeno <arthurbosah@yahoo.com>
2024-10-21 19:25:35 +0100

    clean up timesheet screen

--- Screens/TimesheetEmployeeScreen.jsx
+++ Screens/TimesheetEmployeeScreen.jsx
@@ -1,24 +1,21 @@
 import React, { useRef, useState } from "react";
-import { Timesheet, TimesheetRef } from "@easyteam/ui";
-import Layout from "@/components/Layout";
+import { Timesheet } from "@easyteam/ui";
 
 const TimesheetScreen = () => {
   const [startDate, setStartDate] = useState();
   const [endDate, setEndDate] = useState();
   const ref = useRef(null);
   return (
-    <Layout>
-      <Timesheet
-        ref={ref}
-        onDateRangeChange={(newStartDate, newEndDate) => {
-          setStartDate(newStartDate);
-          setEndDate(newEndDate);
-        }}
-        startDate={startDate}
-        endDate={endDate}
-        onEvent={(event) => console.log(event)}
-      />
-    </Layout>
+    <Timesheet
+      ref={ref}
+      onDateRangeChange={(newStartDate, newEndDate) => {
+        setStartDate(newStartDate);
+        setEndDate(newEndDate);
+      }}
+      startDate={startDate}
+      endDate={endDate}
+      onEvent={(event) => console.log(event)}
+    />
   );
 };
 
