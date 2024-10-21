f5feef3229b5f6881ed54a8cefde706662e78032
kidmeeno <arthurbosah@yahoo.com>
2024-10-21 19:29:24 +0100

    implement logic for the setting screen

--- Screens/SettingsScreen.jsx
+++ Screens/SettingsScreen.jsx
@@ -1,20 +1,40 @@
-import Layout from "@/components/Layout";
 import React from "react";
 import { Settings } from "@easyteam/ui";
-
+import { BASE_URL } from "../config/api";
+import { useAppState } from "../context/AppStateContext";
+import axios from "axios";
 
 const SettingsScreen = () => {
+  const { state } = useAppState();
+  const token = state.token;
+
+  const updateGlobalTrackingSetting = async (payload) => {
+    try {
+      // Make the PUT request with the token for authorization
+      const response = await axios.put(
+        BASE_URL + "/settings",
+        { isGlobalTrackingEnabled: payload },
+        {
+          headers: {
+            Authorization: `auth-token ${token}`, // Pass the token for verification
+            "Content-Type": "application/json", // Ensure proper content type
+          },
+        }
+      );
+      // Handle the response
+      console.log("Updated Setting:", response.data);
+    } catch (error) {
+      console.error("Error updating global tracking setting:", error);
+    }
+  };
+
   return (
-    <Layout>
-      <Settings
-        onSave={({ employees, isGlobalTrackingEnabled }) => {
-          console.log(employees, isGlobalTrackingEnabled);
-        }}
-        onEvent={(event) => console.log(event)}
-      />
-    </Layout>
+    <Settings
+      onSave={({ isGlobalTrackingEnabled }) => {
+        updateGlobalTrackingSetting(isGlobalTrackingEnabled);
+      }}
+    />
   );
 };
-
 
 export default SettingsScreen;
