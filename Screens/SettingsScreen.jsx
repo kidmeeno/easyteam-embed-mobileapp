fa07ade2db402d1eb75e64d62b4f53d561db3151
kidmeeno <arthurbosah@yahoo.com>
2024-10-28 22:32:55 +0100

    use httpClient in setting screen

--- Screens/SettingsScreen.jsx
+++ Screens/SettingsScreen.jsx
@@ -3,12 +3,13 @@ import { Settings } from "@easyteam/ui";
 import { BASE_URL } from "../config/api";
 import axios from "axios";
 import Logger from "../infrastructure/logger/Logger";
+import httpClient from "../infrastructure/httpClient/HttpClient";
 
 const SettingsScreen = () => {
 
   const updateGlobalTrackingSetting = async (payload) => {
     try {
-      await axios.put(
+      await httpClient.put(
         BASE_URL + "/settings",
         { isGlobalTrackingEnabled: payload },
       );
