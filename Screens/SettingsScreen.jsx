58bdd80a6e3856ce3be8c68e635b35e75cc73d1e
kidmeeno <arthurbosah@yahoo.com>
2024-10-28 22:45:59 +0100

    add auth header for token verification on th server side

--- Screens/SettingsScreen.jsx
+++ Screens/SettingsScreen.jsx
@@ -1,17 +1,26 @@
 import React from "react";
 import { Settings } from "@easyteam/ui";
 import { BASE_URL } from "../config/api";
-import axios from "axios";
 import Logger from "../infrastructure/logger/Logger";
 import httpClient from "../infrastructure/httpClient/HttpClient";
+import useAppState from "../state/hooks/useAppState";
 
 const SettingsScreen = () => {
+  const { state } = useAppState();
+  const authToken = state.token;
 
   const updateGlobalTrackingSetting = async (payload) => {
     try {
       await httpClient.put(
         BASE_URL + "/settings",
-        { isGlobalTrackingEnabled: payload },
+        {
+          isGlobalTrackingEnabled: payload,
+        },
+        {
+          headers: {
+            "auth-token": authToken,
+          },
+        }
       );
     } catch (error) {
       Logger.error("Error updating global tracking setting:", error);
